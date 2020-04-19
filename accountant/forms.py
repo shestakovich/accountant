4adb7dcad9f4a13b02124e5c4216dd8e2f4b0769
Anton <shestakovicha@gmail.com>
2020-04-19 15:16:48 +0500

    added SaleForm

---
+++ accountant/forms.py
@@ -0,0 +1,81 @@
+from django import forms
+
+from accountant.models import Sale, SoldService, Client, Service
+
+
+def format_timedelta(timedelta):
+    minutes = timedelta.total_seconds() // 60
+    return f"{minutes // 60}:{minutes % 60}"
+
+
+class SaleForm(forms.ModelForm):
+    client_name = forms.CharField(required=False)
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.request = request
+
+        sold_services = SoldService.objects.filter(sale=self.instance)
+        for i in range(50):
+            self.fields[f"service_{i}"] = forms.CharField(required=False)
+            self.fields[f"amount_{i}"] = forms.IntegerField(min_value=1, required=False)
+            self.fields[f"price_{i}"] = forms.DecimalField(required=False)
+            self.fields[f"lead_time_{i}"] = forms.DurationField(required=False)
+
+        for i, service in enumerate(sold_services):
+            self.initial[f"service_{i}"] = service.name
+            self.initial[f"amount_{i}"] = service.amount
+            self.initial[f"price_{i}"] = service.price
+            self.initial[f"lead_time_{i}"] = format_timedelta(service.lead_time) if service.lead_time else ''
+
+    def clean(self):
+        i = 0
+        sold_services = []
+
+        while self.cleaned_data.get(f"service_{i}"):
+            service_field = f"service_{i}"
+            amount_field = f"amount_{i}"
+            price_field = f"price_{i}"
+            lead_time_field = f"lead_time_{i}"
+
+            sold_services.append({
+                'service': self.cleaned_data.get(service_field),
+                'amount': self.cleaned_data.get(amount_field) or 1,
+                'price': self.cleaned_data.get(price_field),
+                'lead_time': self.cleaned_data.get(lead_time_field) or None,
+            })
+
+            if not self.cleaned_data.get(price_field):
+                self.add_error(price_field, 'required')
+            i += 1
+
+        if not sold_services:
+            self.add_error('service_0', 'required')
+
+        self.cleaned_data['sold_services'] = sold_services
+
+    def save(self, commit=True):
+        if not commit:
+            raise ValueError('In this form commit can only be True')
+
+        sale = super().save(commit=False)
+        sale.company = self.request.user.company
+        sale.service_provider = self.request.user
+        client_name = self.cleaned_data.get('client_name')
+        if client_name:
+            sale.client = Client.objects.get_or_create(company=sale.company, name=client_name)[0]
+        else:
+            sale.client = Client.objects.create(company=sale.company)
+        sale.save()
+
+        for sold_service in self.cleaned_data['sold_services']:
+            sold_service['service'] = Service.objects.get_or_create(company=sale.company,
+                                                                    name=sold_service['service']
+                                                                    )[0]
+            SoldService.objects.create(sale=sale, **sold_service)
+
+        return sale
+
+    class Meta:
+        model = Sale
+        fields = []
--- accountant/tests.py
+++ accountant/tests.py
@@ -1,3 +1,65 @@
+from datetime import timedelta
+
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from accountant.forms import SaleForm
+from accountant.models import Company, SoldService, Service, Sale, Client
+
+User = get_user_model()
+
+
+class SaleFormTests(TestCase):
+    def setUp(self):
+        company = Company.objects.create(name='test_company')
+        self.user = User.objects.create_user(username='test_user', company=company)
+        self.request = lambda x: x
+        self.request.user = self.user
+
+    def test_create_sale_service(self):
+        form = SaleForm(self.request, {
+            'service_0': 'test_service',
+            'price_0': '123.00',
+        })
+
+        self.assertTrue(form.is_valid())
+
+        sale = form.save()
+
+        self.assertEqual(sale.client.name, '')
+        self.assertEqual(sale.services.count(), 1)
+        self.assertEqual(sale.services.all()[0].price, 123.00)
+        self.assertEqual(sale.services.all()[0].service.name, 'test_service')
+        self.assertEqual(sale.services.all()[0].amount, 1)
+        self.assertIsNone(sale.services.all()[0].lead_time)
+
+    def test_no_valid_create_sale(self):
+        form = SaleForm(self.request, {
+            'service_0': 'test_service',
+            'price_0': '123.00',
+            'service_1': 'fail',
+        })
+
+        self.assertFalse(form.is_valid())
+
+        self.assertEqual(form['price_1'].errors[0], 'required')
+
+    def test_create_sale_services(self):
+        form = SaleForm(self.request, {
+            'service_0': 'test_service',
+            'price_0': '123.00',
+            'service_1': 'test_service2',
+            'amount_1': '2',
+            'price_1': '2000000.00',
+            'lead_time_1': '3:00:00',
+        })
+
+        self.assertTrue(form.is_valid())
+
+        sale = form.save()
+
+        self.assertEqual(sale.client.name, '')
+        self.assertEqual(sale.services.count(), 2)
+        self.assertEqual(sale.services.get(service__name='test_service2').price, 2000000.00)
+        self.assertEqual(sale.services.get(service__name='test_service2').amount, 2)
+        self.assertEqual(sale.services.get(service__name='test_service2').lead_time, timedelta(hours=3))
