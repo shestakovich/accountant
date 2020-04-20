7439e95e38454ad5eaf4b1b2a2c89f658dbd4f23
Anton <shestakovicha@gmail.com>
2020-04-20 05:23:58 +0500

    added ClientsPage view, fixed ServicesPage view

--- accountant/urls.py
+++ accountant/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SalesPage, ServicesPage
+from .views import SalesPage, ServicesPage, ClientsPage
 
 urlpatterns = [
     path('sales/', SalesPage.as_view(), name='sales'),
     path('services/', ServicesPage.as_view(), name='services'),
+    path('clients/', ClientsPage.as_view(), name='clients'),
 ]
--- accountant/views.py
+++ accountant/views.py
@@ -1,14 +1,14 @@
 from datetime import timedelta
 
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Sum, FloatField, Avg, Q, Case, When, ExpressionWrapper, F, Value
+from django.db.models import Sum, FloatField, Avg, Q, Case, When, ExpressionWrapper, F, Value, Count, Max
 from django.db.models.functions import Cast, Extract
 from django.shortcuts import render
 from django.urls import reverse
 from django.views.generic import TemplateView, FormView, ListView
 
 from accountant.forms import SaleForm
-from accountant.models import Sale, Service
+from accountant.models import Sale, Service, Client
 
 
 class SalesPage(LoginRequiredMixin, FormView):
@@ -35,27 +35,33 @@ class SalesPage(LoginRequiredMixin, FormView):
         return super().form_valid(form)
 
 
-class ServicesPage(ListView):
+class ServicesPage(LoginRequiredMixin, ListView):
     template_name = 'services.html'
     context_object_name = 'services'
-    queryset = Service.objects \
-        .annotate(sum=Sum('sold__price'),
-                  amount=Sum('sold__amount'),
-                  avg_price=Cast('sum', FloatField()) / Cast('amount', FloatField()),
-                  avg_lead_time=Avg('sold__lead_time', filter=~Q(sold__lead_time=None)),
-                  profitable=Case(When(Q(avg_lead_time__isnull=False) & ~Q(avg_lead_time=timedelta(0)),
-                                       then=ExpressionWrapper(
-                                           F('avg_price') / Extract('avg_lead_time', 'epoch') * 60 * 60, FloatField())),
-                                  default=Value(0),
-                                  output_field=FloatField()
-                                  )
-                  ) \
-        .values('name', 'sum', 'amount', 'avg_lead_time', 'avg_price', 'profitable')
+
+    def get_queryset(self):
+        queryset = Service.objects.filter(company=self.request.user.company) \
+            .annotate(sum=Sum('sold__price'),
+                      amount=Sum('sold__amount'),
+                      avg_price=Cast('sum', FloatField()) / Cast('amount', FloatField()),
+                      avg_lead_time=Avg('sold__lead_time', filter=~Q(sold__lead_time=None)),
+                      profitable=Case(When(Q(avg_lead_time__isnull=False) & ~Q(avg_lead_time=timedelta(0)),
+                                           then=ExpressionWrapper(
+                                               F('avg_price') / Extract('avg_lead_time', 'epoch') * 60 * 60,
+                                               FloatField())),
+                                      default=Value(0),
+                                      output_field=FloatField()
+                                      )
+                      ) \
+            .values('name', 'sum', 'amount', 'avg_lead_time', 'avg_price', 'profitable')
+
+        return queryset
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        most_popular = self.queryset.order_by('-amount')[0]
-        most_rub_in_hour = self.queryset.order_by('-profitable')[0]
-        most_profitable = self.queryset.order_by('-sum')[0]
+        queryset = self.get_queryset()
+        most_popular = queryset.order_by('-amount')[0]
+        most_rub_in_hour = queryset.order_by('-profitable')[0]
+        most_profitable = queryset.order_by('-sum')[0]
 
         context = {
             'most_popular_service': most_popular,
@@ -64,3 +70,19 @@ class ServicesPage(ListView):
         }
         kwargs.update(context)
         return super().get_context_data(object_list=object_list, **kwargs)
+
+
+class ClientsPage(LoginRequiredMixin, ListView):
+    template_name = 'clients.html'
+    context_object_name = 'clients'
+
+    def get_queryset(self):
+        queryset = Client.objects. \
+            filter(company=self.request.user.company) \
+            .annotate(sum=Sum('purchases__services__price'),
+                      amount=Count('purchases'),
+                      last_purchase=Max('purchases__date')
+                      ) \
+            .order_by('-last_purchase')
+
+        return queryset
